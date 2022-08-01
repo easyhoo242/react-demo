a4e7b9080e7cd2584950a9ec875ef2f755e95bf5
Du Cheng <easyhoo2426@163.com>
2022-08-01 17:52:47 +0800

    build: 样式重置

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './pages/index'
+
+// 样式重置
+import '~/assets/style/reset.less'
 import 'uno.css'
 
 const app = ReactDOM.createRoot(document.getElementById('root')!)
