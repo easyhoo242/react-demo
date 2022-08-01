e0a674c17aad7cb87bb112c4c9f678cd898d002d
Du Cheng <easyhoo2426@163.com>
2022-08-01 18:54:04 +0800

    feat(env): 使用env文件

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv, UserConfig, ConfigEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import { join } from 'path'
 import Unocss from 'unocss/vite'
+import { wrapperEnv } from './src/utils'
+
+const root = process.cwd()
+
+const env = loadEnv('development', root)
+const viteEnv = wrapperEnv(env)
+
+const { VITE_PORT, VITE_HOST } = viteEnv
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -20,6 +28,7 @@ export default defineConfig({
     },
   },
   server: {
-    force: true,
+    host: VITE_HOST,
+    port: VITE_PORT,
   },
 })
