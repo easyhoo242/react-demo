fcefd1148562f3e82b5d3e0c23aebe72b0b6874f
Du Cheng <easyhoo2426@163.com>
2022-08-02 21:06:59 +0800

    feat(route): 一级路由

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,49 +1,6 @@
-import { Routes, Route, Link, Router } from 'react-router-dom'
-
-function Home() {
-  return (
-    <>
-      <main>
-        <h2>Welcome to the homepage!</h2>
-        <p>You can do this, I believe in you.</p>
-      </main>
-      <nav>
-        <Link to='/'>index</Link>
-        <Link to='/about'>About</Link>
-      </nav>
-    </>
-  )
-}
-
-function About() {
-  return (
-    <>
-      <main>
-        <h2>Who are we?</h2>
-        <p>That feels like an existential question, don't you think?</p>
-      </main>
-      <nav>
-        <Route path='/about'>
-          <Link to='/'>index</Link>
-          <Link to='/home'>Home</Link>
-        </Route>
-      </nav>
-    </>
-  )
-}
-
-function Main() {
-  return (
-    <>
-      <div>主要</div>
-
-      <div>
-        <Link to={'/home'}> home </Link>
-        <Link to={'/about'}> about </Link>
-      </div>
-    </>
-  )
-}
+import { Routes, Route } from 'react-router-dom'
+import About from '~/pages/About'
+import Home from '~/pages/Home'
 
 export default function App() {
   return (
@@ -51,7 +8,7 @@ export default function App() {
       <header className='bg-full bg-[#f86] p-2'>这是header</header>
 
       <Routes>
-        <Route path='/' element={<Main />} />
+        <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
       </Routes>
     </div>
--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,9 +1,16 @@
-const About = () => {
+import { Link, Route } from 'react-router-dom'
+
+export default function About() {
   return (
     <>
-      <p>About</p>
+      <main>
+        <p>about</p>
+      </main>
+
+      <div>
+        <Link to='/about'>about</Link>
+        <Link to='/'>home</Link>
+      </div>
     </>
   )
 }
-
-export default About
