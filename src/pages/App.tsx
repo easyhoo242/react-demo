1c25a7e360c9e50e20b9b0e5d93ee357eff9e516
Du Cheng <easyhoo2426@163.com>
2022-08-03 13:45:18 +0800

    feat: 加入grid路由

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import About from '~/pages/About'
 import Home from '~/pages/Home'
+import Grid from '~/pages/Grid'
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
+        <Route path='/grid' element={<Grid />} />
       </Routes>
     </div>
   )
---
+++ src/pages/Grid/index.tsx
@@ -0,0 +1,9 @@
+export default function Grid() {
+  return (
+    <>
+      <p>grid</p>
+
+      <div className='container'></div>
+    </>
+  )
+}
